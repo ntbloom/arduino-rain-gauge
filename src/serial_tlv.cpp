d5787330fd4315085e443e76c7f9868f47fc5046
noah.bloom <noah.bloom@everactive.com>
2021-01-26 22:26:47 -0500

    printing serial message for rain gauge, needs refactor

--- src/serial_tlv.cpp
+++ src/serial_tlv.cpp
@@ -2,6 +2,21 @@
 
 using namespace tlv;
 
-void SerialTLV::hex() {
+SerialTLV::SerialTLV(unsigned char* packet, int baud = 9600) {
+    _packet = packet;
+    _baud = baud;
 }
 
+void SerialTLV::_print(int base) {
+    Serial.begin(_baud);
+    for (unsigned char i = 0; i < _packet[1] + 2; i++) {
+        Serial.print(_packet[i], base);
+    }
+    Serial.println();
+    Serial.end();
+}
+
+/* send a TLV packet encoded in hex */
+void SerialTLV::hex() {
+    _print(HEX);
+}
--- src/serial_tlv.hpp
+++ src/serial_tlv.hpp
@@ -7,7 +7,12 @@
 namespace tlv {
 class SerialTLV {
    private:
+    unsigned char* _packet;
+    int _baud;
+    void _print(int base);
+
    public:
+    SerialTLV(unsigned char* packet, int baud);
     void hex();
 };
 };  // namespace tlv
